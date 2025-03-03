a05d2250e0d86744b73632c9dbe537d8be53fb9e
Nightmare0685 <jaxon.s.moraes@gmail.com>
2025-03-03 16:03:50 -0500

    Update IPlayerWaystoneData.java

--- common/src/main/java/net/blay09/mods/waystones/core/IPlayerWaystoneData.java
+++ common/src/main/java/net/blay09/mods/waystones/core/IPlayerWaystoneData.java
@@ -11,6 +11,8 @@ public interface IPlayerWaystoneData {
     void deactivateWaystone(Player player, IWaystone waystone);
     long getWarpStoneCooldownUntil(Player player);
     void setWarpStoneCooldownUntil(Player player, long timeStamp);
+    long getWaystoneCooldownUntil(Player player);
+    void serWaystoneCooldownUntil(Player player, long timeStamp);
     long getInventoryButtonCooldownUntil(Player player);
     void setInventoryButtonCooldownUntil(Player player, long timeStamp);
     List<IWaystone> getWaystones(Player player);
